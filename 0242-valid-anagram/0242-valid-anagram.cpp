be976158c787a212682d71c75624218f94814ad2
Nitin Kumar Shukla <nitinkumarshukla95@gmail.com>
2026-07-21 00:55:16 +0530

    Time: 0 ms (100%), Space: 9.7 MB (76.41%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,12 +1,35 @@
+// class Solution {
+// public:
+//     bool isAnagram(string s, string t) {
+//         if(s.length()!=t.length()){
+//             return false;
+//         }
+//         sort(s.begin(), s.end());
+//         sort(t.begin(), t.end());
+//         if(s==t) return true;
+//         return false;
+//     }
+// };
+
+//2
+
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.length()!=t.length()){
-            return false;
+        vector<int> freq(26, 0);
+
+        for(char c : s){
+            freq[c-'a']++;
         }
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        if(s==t) return true;
-        return false;
+        for(char c : t){
+            freq[c-'a']--;
+        }
+
+        for(int x : freq){
+            if(x != 0){
+                return false;
+            }
+        }
+        return true;
     }
 };
