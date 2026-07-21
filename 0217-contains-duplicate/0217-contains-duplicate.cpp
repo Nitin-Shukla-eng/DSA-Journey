159d22e4800d9b330a9bbfa2f566e970b1519ebe
Nitin Kumar Shukla <nitinkumarshukla95@gmail.com>
2026-07-21 21:24:02 +0530

    Time: 78 ms (19.45%), Space: 111.4 MB (11.59%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,11 +1,26 @@
+// class Solution {
+// public:
+//     bool containsDuplicate(vector<int>& nums) {
+//         sort(nums.begin(),nums.end());
+//         for(int i=0;i<nums.size()-1;i++){
+//             if(nums[i]==nums[i+1]){
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+// };
+
+
+//2
+
 class Solution {
 public:
-    bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size()-1;i++){
-            if(nums[i]==nums[i+1]){
-                return true;
-            }
+    bool containsDuplicate(vector<int> &nums){
+        unordered_set<int> love;
+        for(int n:nums){
+            if(love.count(n)) return true;
+            else love.insert(n);
         }
         return false;
     }
