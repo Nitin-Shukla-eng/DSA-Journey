9e1599a594efd4e82fcd93b12587fad0602a5f37
Nitin Kumar Shukla <nitinkumarshukla95@gmail.com>
2026-08-07 16:46:14 +0530

    Time: 0 ms (100%), Space: 11.5 MB (62.93%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        vector<int> nums = digits;
+
+        for (int i = nums.size() - 1; i >= 0; i--) {
+            if (nums[i] < 9) {
+                nums[i]++;
+                return nums;
+            }
+
+            nums[i] = 0;
+        }
+
+        nums.insert(nums.begin(), 1);
+        return nums;
+    }
+};
