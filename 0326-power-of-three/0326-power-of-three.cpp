c118abdd5d85dafca8b49146f7dbba9cab0c6195
Nitin Kumar Shukla <nitinkumarshukla95@gmail.com>
2026-08-07 17:38:56 +0530

    Time: 0 ms (100%), Space: 8.9 MB (44.14%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -1,14 +1,27 @@
+// class Solution {
+// public:
+//     bool isPowerOfThree(int n) {
+//         if(n<=0) return false;
+//         long long x=1;
+//         while(x<n){
+//             x*=3;
+//         }
+//         if(x==n) return true;
+//         else{
+//              return false;
+//         }
+//     }
+// };
+
 class Solution {
 public:
     bool isPowerOfThree(int n) {
         if(n<=0) return false;
-        long long x=1;
-        while(x<n){
-            x*=3;
+        if(n==1) return true;
+        while(n>0){
+            if(n%3!=0 and n!=1) return false;
+            n/=3;
         }
-        if(x==n) return true;
-        else{
-             return false;
-        }
+        return true;
     }
 };
